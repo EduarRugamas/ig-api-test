686f9703f9bc2c44912c19839ba7ac54ce90a95e
EduarRugamas <juaneduardo021299@hotmail.com>
2022-02-10 12:59:12 -0600

    peticion por axios api instagram

--- src/index.js
+++ src/index.js
@@ -47,18 +47,22 @@ app.get('/login', (req, res) => {
 //ruta Oauth instagram 
 app.get('/instagram/authorize', (req, res) => {
 
-    res.redirect(
-        instagram.getAuthorizationUrl(process.env.IG_URI_REDIRECT, {
-            scope: [
-                'email',
-                'user_profile',
-                'user_photos',
-                'instagram_basic',
-                'instagram_graph_user_profile',
-                'instagram_graph_user_media'
-            ]
-        })
-    );
+    axios.get('https://api.instagram.com/oauth/authorize?client_id=' + process.env.IG_CLIENT_ID + '&redirect_uri=' + process.env.IG_URI_REDIRECT + '&scope=email,user_profile,user_photos,instagram_basic,instagram_graph_user_profile,instagram_graph_user_media&response_type=code')
+        .then(response => { res.render(response); })
+        .catch(error => { res.json(error); });
+
+    // res.redirect(
+    //     instagram.getAuthorizationUrl(process.env.IG_URI_REDIRECT, {
+    //         scope: [
+    //             'email',
+    //             'user_profile',
+    //             'user_photos',
+    //             'instagram_basic',
+    //             'instagram_graph_user_profile',
+    //             'instagram_graph_user_media'
+    //         ]
+    //     })
+    // );
 
 });
 
