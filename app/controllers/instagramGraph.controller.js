0f2bae981cc3118acbf4cbafa950399a5ebd2f01
EduarRugamas <juaneduardo021299@hotmail.com>
2022-02-14 15:57:08 -0600

    resolving bug 2

--- app/controllers/instagramGraph.controller.js
+++ app/controllers/instagramGraph.controller.js
@@ -4,14 +4,14 @@ const localStorage = require('localStorage');
 
 
 
-const getInformationUser = async (req, res) => {
+const getInformationMedia = async (req, res) => {
     const token = localStorage.getItem('access_token');
     const user_id = localStorage.getItem('user_id');
     console.log('user_id: ' + user_id);
     console.log('token obtenido: ' + token);
 
     try {
-        await axios.get('https://graph.instagram.com/' + user_id +'?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=' + token)
+        await axios.get('https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=' + token)
             .then(response => {res.json(response.data);})
             .catch(error => {res.json(error);})
 
@@ -23,5 +23,5 @@ const getInformationUser = async (req, res) => {
 
 
 module.exports = {
-    getInformationUser
+    getInformationMedia
 }
