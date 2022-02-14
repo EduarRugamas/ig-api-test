1af4e534c963e9c8024a126be00f843df6417811
EduarRugamas <juaneduardo021299@hotmail.com>
2022-02-14 13:20:13 -0600

    add respuestas json de axios

--- app/controllers/instagramGraph.controller.js
+++ app/controllers/instagramGraph.controller.js
@@ -8,9 +8,15 @@ const getInformationUser = async function (req, res) {
     const token = localStorage.getItem('access_token');
     console.log('token obtenido' + token);
 
-    const result = await axios.get('https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=' + token, {headers: { 'Content-type': 'application/json ' }});
+    try {
+        const result = await axios.get('https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=' + token, {headers: { 'Content-type': 'application/json ' }});
+        res.json(result.data);
+    }catch (e) {
+        res.json(e.message);
+    }
 
-    res.send(result);
+
+
 };
 
 
