e074d635a6095438d4c5a3f668cc0a3c6ddc2663
EduarRugamas <juaneduardo021299@hotmail.com>
2022-02-14 15:41:59 -0600

    add new peticion axios

--- app/controllers/instagramBasic.controller.js
+++ app/controllers/instagramBasic.controller.js
@@ -14,14 +14,14 @@ const index = (req, res) => {
     res.render('index');
 }
 
-const authorizationWithAxios = (req, res) => {
-    axios.get('https://api.instagram.com/oauth/authorize?client_id=' + config.ig_client_id + '&redirect_uri=' + config.ig_uri_redirect + '&scope=email,user_profile,user_photos,instagram_basic,instagram_graph_user_profile,instagram_graph_user_media&response_type=code').then(response => {
-        console.log(response.data);
-        res.send(response.data);
-    }).catch(error => {
-        res.json(error.message);
-    });
-};
+// const authorizationWithAxios = (req, res) => {
+//     axios.get('https://api.instagram.com/oauth/authorize?client_id=' + config.ig_client_id + '&redirect_uri=' + config.ig_uri_redirect + '&scope=email,user_profile,user_photos,instagram_basic,instagram_graph_user_profile,instagram_graph_user_media&response_type=code').then(response => {
+//         console.log(response.data);
+//         res.send(response.data);
+//     }).catch(error => {
+//         res.json(error.message);
+//     });
+// };
 
 const authorization = (req, res) => {
     res.redirect(
@@ -39,17 +39,17 @@ const authorization = (req, res) => {
     );
 };
 
-const userAuthorizationWithAxios = async (req, res) => {
-    const code = req.query.code;
-    try {
-        const response = await axios.post('https://api.instagram.com/oauth/access_token?client_id='+ config.ig_client_id+'&client_secret='+config.ig_client_secret+'&grant_type=authorization_code&redirect_uri='+config.ig_uri_redirect+'&code='+code);
-        console.log(response);
-        res.json(response);
-    }catch (e) {
-        res.json(e)
-    }
-
-};
+// const userAuthorizationWithAxios = async (req, res) => {
+//     const code = req.query.code;
+//     try {
+//         const response = await axios.post('https://api.instagram.com/oauth/access_token?client_id='+ config.ig_client_id+'&client_secret='+config.ig_client_secret+'&grant_type=authorization_code&redirect_uri='+config.ig_uri_redirect+'&code='+code);
+//         console.log(response);
+//         res.json(response);
+//     }catch (e) {
+//         res.json(e)
+//     }
+//
+// };
 
 const userAuthorization = async (req, res) => {
     try {
--- app/controllers/instagramGraph.controller.js
+++ app/controllers/instagramGraph.controller.js
@@ -6,10 +6,12 @@ const localStorage = require('localStorage');
 
 const getInformationUser = async (req, res) => {
     const token = localStorage.getItem('access_token');
-    console.log('token obtenido' + token);
+    const user_id = localStorage.getItem('user_id');
+    console.log('user_id: ' + user_id);
+    console.log('token obtenido: ' + token);
 
     try {
-        await axios.get('https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=' + token)
+        await axios.get('https://graph.instagram.com/' + user_id +'?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=' + token)
             .then(response => {res.json(response.data);})
             .catch(error => {res.json(error);})
 
